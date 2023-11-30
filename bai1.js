cb4bda70440335bb6c33b67f3e0253be9874dbcf
thanh tuan <pro03tuan.com>
2023-11-30 19:29:57 +0700

    thu

---
+++ bai1.js
@@ -0,0 +1,51 @@
+
+/*
+function ham(){
+    alert("xin chào");
+}
+ham();
+function tinhtong(a,b){
+    var tong = a + b;
+    alert("giá trị tổng là :" + tong);
+}
+tinhtong(6,5);
+ 
+function t(a,b){
+    return a + b;
+}
+
+var tong = t(9,9);
+alert("tổng giá trị là :" + tong);
+*/
+/*
+try {
+    //đoán code có khả năng gây ra lỗi
+    //nếu có lỗi thì đi xuống phần cath
+}
+cath(e){
+    //xử lí lỗi code chạy khi có lỗi
+   alert( e.messsage) //thông báo lỗi
+}
+fanally{
+    // code chạy  cho dù có lỗi hay không có lỗi
+}
+*/
+// ----------------------------------------------
+/*
+function t(a,b){
+    if(b%2==0)
+    {
+        throw "không thể cong lai"
+    }
+    t =  a + b;
+    return t;
+}
+
+try{
+    var tong = t(9,9);
+   alert('tổng giá trị là :' + tong);
+}catch(e){
+  alert(e);// e.message : thông báo lỗi
+}
+*/
+// ----------------------------------------------
